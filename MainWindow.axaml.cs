4be1ad7101ffdcc7ba55386bfbe1988093103f43
Caleb Bennett <serenityvalley06@gmail.com>
2023-01-09 09:12:10 -0600

    Add files via upload

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MirrorMirror
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
