f3ecfd6f3b4edc7a013cb0de2965502943f58b07
Caleb Bennett <cbennett68864@gmail.com>
2023-01-11 22:48:48 -0600

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	modified:   MainWindow.axaml.cs 	modified:   MirrorMirror.csproj 	new file:   OpenWeatherAPI.cs

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -14,8 +14,13 @@ namespace MirrorMirror
 
         private async Task InitializeComponentAsync()
         {
+<<<<<<< HEAD
             //WeatherData data = await OpenWeatherAPI.GetWeatherData("68944");
             //Console.WriteLine($"Temperature in {data.Name} is {data.Temperature} C");
+=======
+            WeatherData data = await OpenWeatherAPI.GetWeatherData("68944");
+            Console.WriteLine($"Temperature in {data.Name} is {data.Temperature} C");
+>>>>>>> d0f9fcb (	new file:   .vscode/launch.json)
             // Add any other initialization code that should be performed after the weather data has been fetched here
             InitializeComponent();
         }
