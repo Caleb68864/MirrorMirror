c66ee05c4ef071fcf036fe4479450f2d6134e8c4
Caleb Bennett <serenityvalley06@gmail.com>
2023-01-11 22:51:59 -0600

    Add files via upload

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Avalonia.Controls;
 
 namespace MirrorMirror
@@ -6,6 +9,14 @@ namespace MirrorMirror
     {
         public MainWindow()
         {
+            InitializeComponentAsync().Wait();
+        }
+
+        private async Task InitializeComponentAsync()
+        {
+            WeatherData data = await OpenWeatherAPI.GetWeatherData("68944");
+            Console.WriteLine($"Temperature in {data.Name} is {data.Temperature} C");
+            // Add any other initialization code that should be performed after the weather data has been fetched here
             InitializeComponent();
         }
     }
