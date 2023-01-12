b532883a701a6fba68ed81806607f590339ab5af
Caleb Bennett <serenityvalley06@gmail.com>
2023-01-12 08:48:02 -0600

    Add files via upload

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -14,8 +14,8 @@ namespace MirrorMirror
 
         private async Task InitializeComponentAsync()
         {
-            WeatherData data = await OpenWeatherAPI.GetWeatherData("68944");
-            Console.WriteLine($"Temperature in {data.Name} is {data.Temperature} C");
+            //WeatherData data = await OpenWeatherAPI.GetWeatherData("68944");
+            //Console.WriteLine($"Temperature in {data.Name} is {data.Temperature} C");
             // Add any other initialization code that should be performed after the weather data has been fetched here
             InitializeComponent();
         }
