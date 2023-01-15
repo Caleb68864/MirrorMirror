07d21e3fab3771a350ee46aa5046fde9525d75ac
Caleb Bennett <cbennett68864@gmail.com>
2023-01-14 22:53:04 -0600

    modified:   App.axaml 	modified:   App.axaml.cs 	deleted:    LICENSE 	deleted:    MainWindow.axaml 	deleted:    MainWindow.axaml.cs 	deleted:    MirrorMirror.csproj 	deleted:    MirrorMirror.sln 	modified:   OpenWeatherAPI.cs 	modified:   Program.cs

--- OpenWeatherAPI.cs
+++ OpenWeatherAPI.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using System.Threading.Tasks;
 using System.Net.Http;
@@ -39,46 +38,4 @@ class OpenWeatherAPI
             }
         }
     }
-=======
-using System;
-using System.Threading.Tasks;
-using System.Net.Http;
-using Newtonsoft.Json;
-
-class WeatherData
-{
-    public string Name { get; set; }
-    public double Temperature { get; set; }
-    public double Humidity { get; set; }
-    public double WindSpeed { get; set; }
-}
-
-class OpenWeatherAPI
-{
-    private const string API_KEY = "bddf0cd56c584112a50d217a543ee815";
-    private const string API_URL = "https://api.openweathermap.org/data/2.5/weather";
-
-    public static async Task<WeatherData> GetWeatherData(string city)
-    {
-        using (HttpClient client = new HttpClient())
-        {
-            string url = $"{API_URL}?q={city}&appid={API_KEY}&units=metric";
-            // string url = "https://random-data-api.com/api/v2/users?size=2&is_xml=true";
-            // Console.WriteLine(url);
-            // string json = await client.GetStringAsync(url);
-            // Console.WriteLine(json);
-            // return JsonConvert.DeserializeObject<WeatherData>(json);
-            using (var response = await client.GetAsync(url))
-            {
-                if (!response.IsSuccessStatusCode)
-                {
-                    Console.WriteLine("Error: " + response.ReasonPhrase);
-                    return null;
-                }
-                var json = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<WeatherData>(json);
-            }
-        }
-    }
->>>>>>> d0f9fcb (	new file:   .vscode/launch.json)
 }
--- Program.cs
+++ Program.cs
@@ -1,23 +1,22 @@
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
 using System;
 
-namespace MirrorMirror
-{
-    internal class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+namespace MirrorMirrorMvvm;
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .LogToTrace();
-    }
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace()
+            .UseReactiveUI();
 }
